82a8be6912e49bed82f3c7d9215a9eacbab185d8
Pratham-fursatAI <prathamd549@gmail.com>
2025-04-02 17:19:34 +0530

    [Feat]: Setup the server

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { PORT } from './config/server-config.js';
+import { connectDB } from './config/db.js';
+
+const app=express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//cors
+app.use(cors());
+
+//database connection
+connectDB();
+
+//routes
+
+
+
+app.get('/', (req, res) => {
+    res.send('Complaint Management System Backend');
+});
+
+
+const setup_and_start_server=()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+}
+
+setup_and_start_server();
